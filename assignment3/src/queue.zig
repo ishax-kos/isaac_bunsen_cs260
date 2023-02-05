0c5723a86d8728cc92dcb645ba0a2993d290c724
ishax-kos <isaacbunsen@gmail.com>
2023-02-04 22:57:16 -0800

    Fix pop_front

--- assignment3/src/queue.zig
+++ assignment3/src/queue.zig
@@ -6,7 +6,6 @@ pub fn Queue(comptime T: type) type {
     return struct {
         head: ?*Node(T),
         tail: ?*Node(T),
-        // allocator: *Allocator,
 
 
         pub fn new_empty() Queue(T) {
@@ -29,19 +28,30 @@ pub fn Queue(comptime T: type) type {
             var new_node: *Node(T) = try allocator.create(Node(T));
             new_node.next = self.tail;
             new_node.value = value;
-            if (self.tail) |*tail| {
-                (tail.*.next) = new_node;
+            if (self.tail) |tail| {
+                tail.next = new_node;
             } else {
                 self.head = new_node;
             }
             self.tail = new_node;
         }
 
-        pub fn pop_front(self:Queue(T)) void {
-            // self.head
-            var old_head = self.head orelse {return;};
-            self.head = self.head.next;
-            allocator.free(old_head);
+        pub fn pop_front(self: *Queue(T)) void {
+            if (self.head) |head| {
+                const tail = self.tail.?;
+                if (head == tail) {
+                    // If there is only one element left in the queue, null it out.
+                    allocator.free(head);
+                    allocator.free(tail);
+                    self.head = null;
+                    self.tail = null;
+                }
+                else {
+                    var old_head = head;
+                    self.head = head.next;
+                    allocator.free(old_head);
+                }
+            }
         }
     };
 }
@@ -62,6 +72,7 @@ test "see if anything works" {
     var queue: Queue(i32) = Queue(i32).new_empty();
     try queue.push_back(55);
     try queue.push_back(2);
+    queue.pop_front();
     try expect(queue.front().? == 55);
     try expect(queue.front().? == 2);
 }
